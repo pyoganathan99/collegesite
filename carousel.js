ccf7a93b06f180ea58faeb4fe623c3205b41dd8a
Yoganathan Palaniswamy <pyoganathan99@gmail.com>
2018-02-06 21:32:01 +0530

    Add files via upload

---
+++ carousel.js
@@ -0,0 +1,32 @@
+
+var mTimeOut;
+
+var slideIndex = 1;
+mTimeOut = setTimeout(function(){ plusSlides(1); }, 3000);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+
+  clearTimeout(mTimeOut);
+  mTimeOut=setTimeout(function(){ plusSlides(1); }, 3000);
+}
